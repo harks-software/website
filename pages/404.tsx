dd05a9c89a560e4ef7037d18b4fb0687968e0b07
AmeerHamoodi <ahamoodi178@gmail.com>
2023-05-12 22:45:11 -0400

    feat: added privacy policy and 404

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import { MainLayout } from "../layouts/main";
+
+export default function LostPage() {
+    return (
+        <MainLayout bottomPadding={false}>
+            <div className="flex flex-col items-center justify-center w-full h-96 bg-cream">
+                <h1 className="font-bold text-7xl w-full text-center">404</h1>
+                <h2 className="text-4xl font-bold w-full text-center">
+                    We can&apos;t seem to find what you&apos;re looking for
+                </h2>
+            </div>
+        </MainLayout>
+    );
+}
