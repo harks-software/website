94fee402f37ff9ad3c0ee5465bb87ce37772a017
SmokeTrails <mohsin.reza@mail.utoronto.ca>
2023-05-13 23:23:48 -0400

    feat: about us page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,14 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "@/styles/explore-slideshow.css";
 import "@/styles/testimonials.css";
+import "@/styles/about-us.css";
 
 import { openSans } from "../utils/fonts";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <main className={`${openSans.className}`}>
-            <Component {...pageProps} />
-        </main>
-    );
+  return (
+    <main className={`${openSans.className}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
