22273b8d399103acc4270f8bca2bbb2b463ad4aa
AmeerHamoodi <ahamoodi178@gmail.com>
2023-06-03 21:55:30 -0400

    fix: redirect to successful donate when successful part2

---
+++ pages/successful-donation.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { MainLayout } from "../layouts/main";
+import { montserrat } from "../utils/fonts";
+import DonateComponent from "../components/donate";
+import { PreviousWork } from "../components/donate/previous-work";
+import { CounterNumericDisplay } from "../components/home/counter";
+
+const PREVIOUS_WORK_IMAGES = [
+    { image: "./assets/donate/HARKS-naynawah.jpg" },
+    { image: "./assets/donate/youthday-thumbnail.png" },
+];
+
+export default function Donate() {
+    const [tab, setTab] = useState(0);
+
+    return (
+        <MainLayout bottomPadding={false}>
+            <div className={`${montserrat.className} font-bold text-3xl text-center py-24`}>
+                Thank you for your donation
+            </div>
+        </MainLayout>
+    );
+}
