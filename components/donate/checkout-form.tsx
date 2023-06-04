93258eff0a18e38208c1ce29ab495424ea87ecfa
AmeerHamoodi <ahamoodi178@gmail.com>
2023-06-03 21:52:18 -0400

    fix: redirect to successful donate when successful

--- components/donate/checkout-form.tsx
+++ components/donate/checkout-form.tsx
@@ -59,7 +59,7 @@ export default function CheckoutForm({ setClientSecret }: ICheckoutFormProps) {
             elements,
             confirmParams: {
                 // Make sure to change this to your payment completion page
-                return_url: window.location.protocol + "//hussainsark.com/copy-of-donate",
+                return_url: window.location.host + "/donate",
             },
         });
 
