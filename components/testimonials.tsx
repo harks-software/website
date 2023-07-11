247a0ced54500832ba5d2c4258397c6f20ada1b0
Saahil Jaffer <contact@saahiljaffer.com>
2023-07-10 22:29:36 -0700

    chore: add prettier (#19)
    * chore: add prettier
    
    * chore: run prettier

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -4,92 +4,93 @@ import { Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 interface IQuote {
-    content: string;
-    quoted: string;
-    img: string;
+  content: string;
+  quoted: string;
+  img: string;
 }
 
 interface ITestimonialsProps {
-    list: IQuote[];
+  list: IQuote[];
 }
 
 export function Testimonials({ list }: ITestimonialsProps) {
-    return (
-        <Swiper
-            className="testimonials"
-            spaceBetween={0}
-            loop={true}
-            centeredSlides={true}
-            speed={500}
-            autoplay={{
-                delay: 10000,
-                disableOnInteraction: false,
-            }}
-            slidesPerView={1}
-            modules={[Autoplay]}
-            draggable={false}
-            allowTouchMove={false}
-        >
-            {list.map((item) => (
-                <SwiperSlide key={item.content}>
-                    {/* mobile */}
-                    <div className="relative rounded-2xl bg-white py-6 px-4 md:hidden">
-                        <i className="fa-solid fa-quote-left absolute left-0 top-36 text-2xl text-[#E7AC37]"></i>
-                        <i className="fa-solid fa-quote-left absolute right-2 bottom-2 rotate-180 text-2xl text-[#E7AC37]"></i>
-                        <div className="flex w-full items-center">
-                            <div
-                                className="my-2 mr-4 flex h-24 w-24 items-center justify-center rounded-full"
-                                style={{
-                                    background: "linear-gradient(180deg, #BE1E2D 0%, #D29923 100%)",
-                                }}
-                            >
-                                <div
-                                    className="h-[90px] w-[90px] rounded-full bg-white/70 bg-cover bg-center"
-                                    style={{
-                                        backgroundImage: `url('/assets/home/testimonials/${item.img}')`,
-                                    }}
-                                ></div>
-                            </div>
-                            <div className="font-montserrat text-lg font-medium md:text-2xl">
-                                {item.quoted}
-                            </div>
-                        </div>
+  return (
+    <Swiper
+      className="testimonials"
+      spaceBetween={0}
+      loop={true}
+      centeredSlides={true}
+      speed={500}
+      autoplay={{
+        delay: 10000,
+        disableOnInteraction: false,
+      }}
+      slidesPerView={1}
+      modules={[Autoplay]}
+      draggable={false}
+      allowTouchMove={false}
+    >
+      {list.map((item) => (
+        <SwiperSlide key={item.content}>
+          {/* mobile */}
+          <div className="relative rounded-2xl bg-white py-6 px-4 md:hidden">
+            <i className="fa-solid fa-quote-left absolute left-0 top-36 text-2xl text-[#E7AC37]"></i>
+            <i className="fa-solid fa-quote-left absolute right-2 bottom-2 rotate-180 text-2xl text-[#E7AC37]"></i>
+            <div className="flex w-full items-center">
+              <div
+                className="my-2 mr-4 flex h-24 w-24 items-center justify-center rounded-full"
+                style={{
+                  background:
+                    "linear-gradient(180deg, #BE1E2D 0%, #D29923 100%)",
+                }}
+              >
+                <div
+                  className="h-[90px] w-[90px] rounded-full bg-white/70 bg-cover bg-center"
+                  style={{
+                    backgroundImage: `url('/assets/home/testimonials/${item.img}')`,
+                  }}
+                ></div>
+              </div>
+              <div className="font-montserrat text-lg font-medium md:text-2xl">
+                {item.quoted}
+              </div>
+            </div>
 
-                        <div className="mt-6 flex flex-1 flex-col text-center align-middle">
-                            <div className="px-2 text-center font-montserrat text-sm font-normal md:text-xl">
-                                {item.content}
-                            </div>
-                        </div>
-                    </div>
-                    {/* desktop */}
-                    <div className="items.center relative hidden content-evenly rounded-2xl bg-white py-6 px-4 md:flex">
-                        <i className="fa-solid fa-quote-left absolute top-0 left-28 text-5xl text-[#E7AC37]"></i>
-                        <i className="fa-solid fa-quote-left absolute bottom-0 rotate-180 text-5xl text-[#E7AC37] md:right-8 lg:right-28"></i>
-                        <div
-                            className="my-auto mr-4 flex h-24 w-24 items-center justify-center rounded-full"
-                            style={{
-                                background: "linear-gradient(180deg, #BE1E2D 0%, #D29923 100%)",
-                            }}
-                        >
-                            <div
-                                className="h-[90px] w-[90px] rounded-full bg-white/70 bg-cover bg-center"
-                                style={{
-                                    backgroundImage: `url('/assets/home/testimonials/${item.img}')`,
-                                }}
-                            ></div>
-                        </div>
+            <div className="mt-6 flex flex-1 flex-col text-center align-middle">
+              <div className="px-2 text-center font-montserrat text-sm font-normal md:text-xl">
+                {item.content}
+              </div>
+            </div>
+          </div>
+          {/* desktop */}
+          <div className="items.center relative hidden content-evenly rounded-2xl bg-white py-6 px-4 md:flex">
+            <i className="fa-solid fa-quote-left absolute top-0 left-28 text-5xl text-[#E7AC37]"></i>
+            <i className="fa-solid fa-quote-left absolute bottom-0 rotate-180 text-5xl text-[#E7AC37] md:right-8 lg:right-28"></i>
+            <div
+              className="my-auto mr-4 flex h-24 w-24 items-center justify-center rounded-full"
+              style={{
+                background: "linear-gradient(180deg, #BE1E2D 0%, #D29923 100%)",
+              }}
+            >
+              <div
+                className="h-[90px] w-[90px] rounded-full bg-white/70 bg-cover bg-center"
+                style={{
+                  backgroundImage: `url('/assets/home/testimonials/${item.img}')`,
+                }}
+              ></div>
+            </div>
 
-                        <div className="mt-6 flex flex-1 flex-col text-center align-middle">
-                            <div className="text-center font-montserrat text-sm font-normal md:text-xl">
-                                {item.content}
-                            </div>
-                            <div className="pt-4 font-montserrat text-lg font-medium md:text-2xl">
-                                {item.quoted}
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>
-            ))}
-        </Swiper>
-    );
+            <div className="mt-6 flex flex-1 flex-col text-center align-middle">
+              <div className="text-center font-montserrat text-sm font-normal md:text-xl">
+                {item.content}
+              </div>
+              <div className="pt-4 font-montserrat text-lg font-medium md:text-2xl">
+                {item.quoted}
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,67 +2,67 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-        "./layouts/**/*.{js,tsx,jsx,tsx}",
-    ],
-    theme: {
-        fontFamily: {
-            sans: ["var(--font-open-sans)", ...fontFamily.sans],
-            body: ["var(--font-open-sans)"],
-        },
-        extend: {
-            colors: {
-                "yellow.700": "#554402",
-                "yellow.600": "#AA8805",
-                "yellow.500": "#FFCC07",
-                "yellow.400": "#FFE583",
-                "yellow.300": "#FFEEAC",
-                "red.700": "#400A0F",
-                "red.600": "#7F141F",
-                "red.500": "#BF1E2E",
-                "red.400": "#D47B84",
-                "red.300": "#EAA8AF",
-                "orange.500": "#DF761A",
-                "gray.800": "#282828",
-                "beige.700": "#3E3729",
-                "beige.600": "#7C6E52",
-                "beige.500": "#B9A47A",
-                "beige.400": "#F7DBA3",
-                "beige.300": "#f0e4d4",
-                "beige.200": "#F5F2EB",
-                "gold.700": "#4D3912",
-                "gold.600": "#9B7323",
-                "gold.500": "#D29923",
-                "gold.400": "#ECBE61",
-                "gold.300": "#F7E2B8",
-                mustard: "#E7AD36",
-                cream: "#EBE4D6",
-            },
-            fontFamily: {
-                oswald: "var(--font-oswald)",
-                montserrat: "var(--font-montserrat)",
-                bebas: "var(--font-bebas-neue)",
-                architects: "var(--font-architects-daughter)",
-            },
-            animation: {
-                fade15: "fadeIn 1.5s ease-in-out",
-            },
-            keyframes: () => ({
-                fadeIn: {
-                    "0%": { opacity: 0 },
-                    "100%": { opacity: 1 },
-                },
-            }),
-        },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,tsx,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["var(--font-open-sans)", ...fontFamily.sans],
+      body: ["var(--font-open-sans)"],
     },
-    plugins: [
-        require("@tailwindcss/aspect-ratio"),
-        require("@tailwindcss/typography"),
-        require("daisyui"),
-    ],
-    daisyui: {
-        themes: [],
+    extend: {
+      colors: {
+        "yellow.700": "#554402",
+        "yellow.600": "#AA8805",
+        "yellow.500": "#FFCC07",
+        "yellow.400": "#FFE583",
+        "yellow.300": "#FFEEAC",
+        "red.700": "#400A0F",
+        "red.600": "#7F141F",
+        "red.500": "#BF1E2E",
+        "red.400": "#D47B84",
+        "red.300": "#EAA8AF",
+        "orange.500": "#DF761A",
+        "gray.800": "#282828",
+        "beige.700": "#3E3729",
+        "beige.600": "#7C6E52",
+        "beige.500": "#B9A47A",
+        "beige.400": "#F7DBA3",
+        "beige.300": "#f0e4d4",
+        "beige.200": "#F5F2EB",
+        "gold.700": "#4D3912",
+        "gold.600": "#9B7323",
+        "gold.500": "#D29923",
+        "gold.400": "#ECBE61",
+        "gold.300": "#F7E2B8",
+        mustard: "#E7AD36",
+        cream: "#EBE4D6",
+      },
+      fontFamily: {
+        oswald: "var(--font-oswald)",
+        montserrat: "var(--font-montserrat)",
+        bebas: "var(--font-bebas-neue)",
+        architects: "var(--font-architects-daughter)",
+      },
+      animation: {
+        fade15: "fadeIn 1.5s ease-in-out",
+      },
+      keyframes: () => ({
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      }),
     },
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: [],
+  },
 };
