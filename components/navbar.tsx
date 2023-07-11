e10a533b8b0f04ed4b3deb9378099c0e773d11ff
Saahil Jaffer <contact@saahiljaffer.com>
2023-07-10 22:26:56 -0700

    chore: update footer content (#20)
    * chore: update footer content
    
    * chore: remove podcast
    
    * chore: more footer and navbar changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ export function Navbar() {
   return (
     <div className="fixed top-0 left-0 z-[10000] w-full">
       <div
-        className="flow-root w-full p-2 drop-shadow-xl"
+        className="flow-root w-full p-2 pr-6 md:pr-8 drop-shadow-xl"
         style={{
           backgroundImage: "linear-gradient(0deg, #1A1A1A, #1A1A1A)",
         }}
@@ -13,16 +13,18 @@ export function Navbar() {
         <div className="float-left ml-2 text-sm text-white md:ml-4">
           Hussain&apos;s Ark of Salvation
         </div>
-        <div className="float-right flex ">
+        <div className="float-right flex gap-3">
           <Link
             href="https://www.instagram.com/hussainsark"
             className="transition-all duration-200 hover:opacity-60"
+            target="_blank"
           >
             <svg
               role="img"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
-              className="mr-3 h-4 md:mr-3 md:h-5"
+              className="h-4 md:h-5"
+              target="_blank"
             >
               <title>Instagram</title>
               <path
@@ -34,12 +36,13 @@ export function Navbar() {
           <Link
             href="https://facebook.com/hussainsark"
             className="transition-all duration-200 hover:opacity-60"
+            target="_blank"
           >
             <svg
               role="img"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
-              className="mr-3 h-4 fill-white md:mr-3 md:h-5"
+              className="h-5 fill-white"
             >
               <title>Facebook</title>
               <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"></path>
@@ -48,12 +51,13 @@ export function Navbar() {
           <Link
             href="https://www.youtube.com/@hussainsark"
             className="transition-all duration-200 hover:opacity-60"
+            target="_blank"
           >
             <svg
               role="img"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
-              className="mr-3 h-4 fill-white md:mr-3 md:h-5"
+              className="h-5 fill-white"
             >
               <title>YouTube</title>
               <path
@@ -65,15 +69,45 @@ export function Navbar() {
           <Link
             href="https://tiktok.com/@hussainsark"
             className="transition-all duration-200 hover:opacity-60"
+            target="_blank"
           >
             <svg
               role="img"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
-              className="mr-3 h-4 fill-white md:mr-3 md:h-5"
+              className="h-5 fill-white"
             >
               <title>TikTok</title>
               <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z"></path>
+            </svg>
+          </Link>
+          <Link
+            className="flex place-content-center text-xs italic transition-all duration-200 hover:opacity-60"
+            href="https://linkedin.com/company/harks"
+            target="_blank"
+          >
+            <svg
+              role="img"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 fill-white"
+            >
+              <title>LinkedIn</title>
+              <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"></path>
+            </svg>
+          </Link>
+          <Link
+            className="flex place-content-center text-xs italic transition-all duration-200 hover:opacity-60"
+            href="/whatsapp-broadcasting"
+          >
+            <svg
+              role="img"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 fill-white"
+            >
+              <title>WhatsApp</title>
+              <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z"></path>
             </svg>
           </Link>
         </div>
@@ -93,7 +127,7 @@ export function Navbar() {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
-                strokeWidth="3"
+                stroke-width="3"
                 stroke="white"
                 className="w-612 float-right m-2 h-8"
               >
@@ -136,9 +170,6 @@ export function Navbar() {
                 <ul className="bg-white">
                   <li className="cursor-pointer rounded-lg transition-all duration-300 hover:bg-gray-300">
                     <Link href="/youth-day">Youth Day</Link>
-                  </li>
-                  <li className="cursor-pointer rounded-lg transition-all duration-300 hover:bg-gray-300">
-                    <Link href="/podcast">The HARKS Podcast</Link>
                   </li>
                   <li className="cursor-pointer rounded-lg transition-all duration-300 hover:bg-gray-300">
                     <Link href="/majlis-shorts">Majlis Shorts</Link>
@@ -259,9 +290,6 @@ export function Navbar() {
                 <Link href="/youth-day">Youth Day</Link>
               </li>
               <li className="cursor-pointer rounded-lg font-bold transition-all duration-300 hover:bg-gray-300">
-                <Link href="/podcast">The HARKS Podcast</Link>
-              </li>
-              <li className="cursor-pointer rounded-lg font-bold transition-all duration-300 hover:bg-gray-300">
                 <Link href="/majlis-shorts">Majlis Shorts</Link>
               </li>
             </ul>
@@ -332,9 +360,9 @@ export function Navbar() {
           </Link>
           <Link
             href="/donate"
-            className="mr-4 flex cursor-pointer whitespace-nowrap rounded-2xl bg-white py-2 px-4 font-bold shadow-md transition-all duration-200 hover:bg-zinc-400/60 md:text-[0.65rem] lg:text-xs"
+            className="mr-4 flex cursor-pointer whitespace-nowrap rounded-2xl bg-white py-2 px-4 font-bold shadow-md transition-all duration-200 hover:bg-zinc-400/60 md:text-[0.65rem] lg:text-xs uppercase"
           >
-            DONATE
+            Donate
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
