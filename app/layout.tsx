4da0a0c65a0025dd8dbca4a4b42e645d3cc9a2d0
Saahil Jaffer <contact@saahiljaffer.com>
2023-07-23 13:54:05 -0700

    chore: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat } from "next/font/google";
 import { Navbar } from "./components/navbar";
 import { Footer } from "./components/footer";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Hussain's Ark of Salvation",
@@ -25,6 +26,16 @@ export default function RootLayout({
   return (
     <html lang="en" className={montserrat.className}>
       <body>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-XQL7DBF32L"></Script>
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+        </Script>
         <Navbar />
         {children}
         <Footer />
