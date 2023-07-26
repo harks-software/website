29cc4d9d7dce6bd80690a1a85f2943ab6a88f8bc
Saahil Jaffer <contact@saahiljaffer.com>
2023-07-26 01:56:14 -0700

    feat: add back skeleton

--- app/components/home/calendar-events.tsx
+++ app/components/home/calendar-events.tsx
@@ -1,22 +1,50 @@
 "use client";
 import dayjs from "dayjs";
 import advancedFormat from "dayjs/plugin/advancedFormat";
-import { calendar_v3, google } from "googleapis";
+import { calendar_v3 } from "googleapis";
 import { useEffect, useState } from "react";
 
 dayjs.extend(advancedFormat);
 
 export function CalendarEvents() {
   const [events, setEvents] = useState<calendar_v3.Schema$Event[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       const fetchEvents = await fetch("/api/events");
       const json = await fetchEvents.json();
       const events = json.events as calendar_v3.Schema$Event[];
       setEvents(events);
+      setIsLoading(false);
     }
     fetchData();
   }, []);
+
+  if (isLoading)
+    return (
+      <div className="flex w-[300px] flex-col py-4 sm:w-[450px] lg:w-full">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <div
+            role="status"
+            className="my-4 flex h-[92px] w-full animate-pulse rounded-lg bg-white"
+            key={index}
+          >
+            <div className="mb-4 flex h-full flex-[0.2] flex-col rounded-l-lg bg-[#242424BF] p-1 font-bold sm:p-2">
+              <div className="mb-2.5 h-4 max-w-[80px] rounded-full bg-[#242424BF] dark:bg-white"></div>
+              <div className="mb-2.5 h-6 max-w-[120px] rounded-full bg-[#242424BF] dark:bg-white"></div>
+            </div>
+
+            <div className="flex-1 flex-col p-4">
+              <div className="mb-2.5 h-4 max-w-[100px] rounded-full bg-white dark:bg-[#242424BF]"></div>
+              <div className="mb-2.5 h-6 max-w-[300px] rounded-full bg-white dark:bg-[#242424BF]"></div>
+            </div>
+            <span className="sr-only">Loading...</span>
+          </div>
+        ))}
+      </div>
+    );
 
   return (
     <div className="flex w-[300px] flex-col py-4 sm:w-[450px] lg:w-full">
